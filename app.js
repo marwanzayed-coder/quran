f558ed574b5580c9fecce0b69e090d5cbc8a3a4f
marwanzayed-coder <marwanzayedshaban@gmail.com>
2022-04-02 22:32:37 +0200

    Fix Error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(helmet());
+app.use(helmet());
 app.use(cors());
 
 let city = "cairo";
